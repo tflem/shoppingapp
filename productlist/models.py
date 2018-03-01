913612d4afe86c1f1c742e646f304e94c536d1a4
Tim Fleming <tfleming26@hotmail.com>
2018-03-01 12:17:32 -0600

    Add product model with migrations

--- productlist/models.py
+++ productlist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """
+    Model representing a product (e.g. Deodorant, Toothpaste).
+    """
+    item = models.CharField(max_length=50, help_text="Enter a product item (e.g. Deodorant, Toothpaste etc.)")
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.item
