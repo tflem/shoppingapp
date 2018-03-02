ff4f71c212edacfe593d2ba50775cfe6e5e10c0c
Tim Fleming <tfleming26@hotmail.com>
2018-03-02 09:10:50 -0600

    Register product model

--- productlist/admin.py
+++ productlist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('item', 'quantity')
+
+admin.site.register(Product, ProductAdmin)
