37e8a3c3d5cf8f8f5f88da21537c21f590621eff
Tim Fleming <tfleming26@hotmail.com>
2018-03-03 01:01:16 -0600

    Add class based generic list view

--- productlist/urls.py
+++ productlist/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Define products url pattern
+    path('products/', views.ProductListView.as_view(), name='products'),
 ]
