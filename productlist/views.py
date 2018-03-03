05132f4837cb0961a194d6f7b05f95236dd7d4ee
Tim Fleming <tfleming26@hotmail.com>
2018-03-03 01:03:40 -0600

    Add class-based list view

--- productlist/views.py
+++ productlist/views.py
@@ -5,3 +5,9 @@ def index(request):
     number_of_products = Product.objects.all().count()
     context = {'number_of_products': number_of_products}
     return render(request, 'index.html', context)
+
+# Add Class-Based Generic List View
+from django.views import generic
+
+class ProductListView(generic.ListView):
+    model = Product
