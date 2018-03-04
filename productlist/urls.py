d5a33b583741ff02b45549b491e2c735c5beb5a9
Tim Fleming <tfleming26@hotmail.com>
2018-03-04 15:56:15 -0600

    Define create url pattern

--- productlist/urls.py
+++ productlist/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('products/', views.ProductListView.as_view(), name='products'),
     path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('product/new/', views.ProductCreateView.as_view(), name='product-new'),
 ]
