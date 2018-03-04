0a5cb30076a9487898e2220c9ea48c2b497331e3
Tim Fleming <tfleming26@hotmail.com>
2018-03-04 00:19:55 -0600

    Define url pattern for individual items

--- productlist/urls.py
+++ productlist/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    # Define products url pattern
+    path('', views.IndexView.as_view(), name='index'),
     path('products/', views.ProductListView.as_view(), name='products'),
+    path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
 ]
