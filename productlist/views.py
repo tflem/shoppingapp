b13543158a1bdfeb15eea6d6a3a6ad97893f7c74
Tim Fleming <tfleming26@hotmail.com>
2018-03-04 15:54:02 -0600

    Add template names to remaining class views

--- productlist/views.py
+++ productlist/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views import generic
 
 from .models import Product
@@ -10,12 +9,16 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Product.objects.all().count()
 
-# Add Class-Based Generic List View
-
 class ProductListView(generic.ListView):
-    model = Product
-    # Add pagination
-    paginate_by = 10
+    model         = Product
+    template_name = 'product_list.html'
+    paginate_by   = 10
 
 class ProductDetailView(generic.DetailView):
-    model = Product
+    model         = Product
+    template_name = 'product_detail.html'
+
+class ProductCreateView(generic.CreateView):
+    model         = Product
+    template_name = 'product_new.html'
+    fields        = '__all__'
