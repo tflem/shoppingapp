dca0e66fd2cb7b80c41cd28f3ee81dc3d71bbe3b
Tim Fleming <tfleming26@hotmail.com>
2018-03-04 00:18:09 -0600

    Convert remaining function to generic view

--- productlist/views.py
+++ productlist/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from .models import Product
-
-def index(request):
-    number_of_products = Product.objects.all().count()
-    context = {'number_of_products': number_of_products}
-    return render(request, 'index.html', context)
-
-# Add Class-Based Generic List View
 from django.views import generic
 
+from .models import Product
+
+class IndexView(generic.ListView):
+    template_name       = 'index.html'
+    context_object_name = 'number_of_products'
+
+    def get_queryset(self):
+        return Product.objects.all().count()
+
+# Add Class-Based Generic List View
+
 class ProductListView(generic.ListView):
+    model = Product
+    # Add pagination
+    paginate_by = 10
+
+class ProductDetailView(generic.DetailView):
     model = Product
