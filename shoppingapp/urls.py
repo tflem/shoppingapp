5128bf4012a21b51cdadf078f432dc525115681b
Tim Fleming <tfleming26@hotmail.com>
2018-03-04 15:51:07 -0600

    Remove redirect view

--- shoppingapp/urls.py
+++ shoppingapp/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import RedirectView
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('productlist/', include('productlist.urls')),
-    path('', RedirectView.as_view(url='/productlist/')),
+    path('', include('productlist.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
