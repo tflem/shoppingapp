e8d62057204932f1f2505d29d75e8a381c8e86b8
Tim Fleming <tfleming26@hotmail.com>
2018-03-05 02:30:33 -0600

    Add extra slash in product detail url pattern

--- productlist/urls.py
+++ productlist/urls.py
@@ -4,6 +4,6 @@ from  . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('products/', views.ProductListView.as_view(), name='products'),
-    path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('product/new/', views.ProductCreateView.as_view(), name='product-new'),
 ]
