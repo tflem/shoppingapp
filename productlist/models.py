a012c100f3e567e0d6e7efb1b756cdd61145841a
Tim Fleming <tfleming26@hotmail.com>
2018-03-05 02:34:18 -0600

    Add get absolute url function

--- productlist/models.py
+++ productlist/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Product(models.Model):
     """
@@ -6,6 +7,9 @@ class Product(models.Model):
     """
     item = models.CharField(max_length=50, help_text="Enter a product item (e.g. Deodorant, Toothpaste etc.)")
     quantity = models.IntegerField(default=1)
+
+    def get_absolute_url(self):
+        return reverse('product-detail', args=[str(self.id)])        
 
     def __str__(self):
         return self.item
